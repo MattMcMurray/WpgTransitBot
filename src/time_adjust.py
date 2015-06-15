fa296775f6aadcfd516cf65ae19ff54f8a6d5bd3
MattMcMurray <mmcmurray@visualspection.com>
2015-06-15 17:38:55 -0500

    added an end time

--- src/time_adjust.py
+++ src/time_adjust.py
@@ -5,8 +5,10 @@ from pytz import timezone
 
 def get_wpg_time():
     wpg_tz = timezone('Canada/Central')
-    wpg_time = datetime.now(wpg_tz)
+    wpg_time_start = datetime.now(wpg_tz)
+    wpg_time_end = wpg_time_start + timedelta(hours=1)
 
-    time_str = wpg_time.strftime('%Y-%m-%dT%H:%M:%S')
+    time_str_start = wpg_time_start.strftime('%Y-%m-%dT%H:%M:%S')
+    time_str_end = wpg_time_end.strftime('%Y-%m-%dT%H:%M:%S')
 
-    return time_str
+    return time_str_start, time_str_end
