8c24d8356077e9204e24c734beca6b964608239f
MattMcMurray <mmcmurray@visualspection.com>
2015-07-06 19:47:40 -0500

    time adjust correctly returns Winnipeg time

--- src/time_adjust.py
+++ src/time_adjust.py
@@ -2,11 +2,13 @@ from datetime import datetime, timedelta
 
 from pytz import timezone
 
+TIME_BETWEEN = 1
+
 
 def get_wpg_time():
     wpg_tz = timezone('Canada/Central')
     wpg_time_start = datetime.now(wpg_tz)
-    wpg_time_end = wpg_time_start + timedelta(hours=1)
+    wpg_time_end = wpg_time_start + timedelta(hours=TIME_BETWEEN)
 
     time_str_start = wpg_time_start.strftime('%Y-%m-%dT%H:%M:%S')
     time_str_end = wpg_time_end.strftime('%Y-%m-%dT%H:%M:%S')
