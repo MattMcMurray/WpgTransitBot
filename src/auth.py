f2a794d43844d80965f6bd4cf15f23020502ee59
MattMcMurray <mathieumcmurray@gmail.com>
2015-10-02 11:28:18 -0500

    Created a 'Key' class to store auth information

--- src/auth.py
+++ src/auth.py
@@ -1,8 +1,28 @@
 import tweepy
 
-from secrets import ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
+# from secrets import ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
 from services import printlog
 
+class Key:
+    def __init__(self, access_token, access_secret, consumer_key, consumer_secret):
+        self.access_token = access_token
+        self.access_secret = access_secret
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+
+def getUserCreds():
+    a_token = raw_input("Please enter your access token:\n")
+    a_secret = raw_input("Please enter your access secret:\n")
+    c_key = raw_input("Please enter your consumer key:\n")
+    c_secret = raw_input("Please enter your consumer secret:\n")
+
+    key = Key(a_token, a_secret, c_key, c_secret)
+
+    print 'Object instatiated'
+    print key.access_token
+    print key.access_secret
+    print key.consumer_key
+    print key.consumer_secret
 
 def authenticate():
     try:
@@ -18,4 +38,5 @@ def authenticate():
         printlog(e.message)
         return False
 
-API = authenticate()
+# API = authenticate()
+getUserCreds()
