59c9d47c00134c147830676edb523659dc783710
MattMcMurray <mathieumcmurray@gmail.com>
2015-10-02 11:55:55 -0500

    Use pickle to load/save the user's key

--- src/auth.py
+++ src/auth.py
@@ -1,6 +1,8 @@
+import os
 import tweepy
+import pickle
+import pprint
 
-# from secrets import ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
 from services import printlog
 
 class Key:
@@ -11,32 +13,51 @@ class Key:
         self.consumer_secret = consumer_secret
 
 def getUserCreds():
+    print '*********************'
+    print '*** Twitter OAuth ***'
+    print '*********************'
     a_token = raw_input("Please enter your access token:\n")
     a_secret = raw_input("Please enter your access secret:\n")
     c_key = raw_input("Please enter your consumer key:\n")
     c_secret = raw_input("Please enter your consumer secret:\n")
 
     key = Key(a_token, a_secret, c_key, c_secret)
+    printlog("Instatiated key object")
 
-    print 'Object instatiated'
-    print key.access_token
-    print key.access_secret
-    print key.consumer_key
-    print key.consumer_secret
+    return key
 
 def authenticate():
-    try:
-        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-        api = tweepy.API(auth)
+    print 'authenticating'
+    # get the absolute path of this file to avoid relative path weirdness
+    here = os.path.dirname(os.path.abspath(__file__))
+    resource_dir = '{0}/../res/'.format(here)
+    key_filename = 'key.pkl'
 
-        #TODO fix; authentication always returns OK
-        return api
+    if not os.path.exists(resource_dir):
+        os.makedirs(resource_dir)
 
-    except tweepy.TweepError as e:
-    	printlog("Tweepy Error while autheticating")
-        printlog(e.message)
-        return False
+    if os.path.isfile(resource_dir + key_filename):
+        key_file = open(resource_dir+key_filename, 'r')
+        key = pickle.load(key_file)
+
+    else:
+        key = getUserCreds()
+        key_file = open(resource_dir+key_filename, 'wb')
+        pickle.dump(key, key_file)
+
+
+        # try:
+        #     auth = tweepy.OAuthHandler(key.consumer_key, key.consumer_secret)
+        #     auth.set_access_token(key.access_token, key.access_secret)
+        #     api = tweepy.API(auth)
+
+        #     #TODO fix; authentication always returns OK
+        #     return api
+
+        # except tweepy.TweepError as e:
+        # 	printlog("Tweepy Error while autheticating")
+        #     printlog(e.message)
+        #     return False
 
 # API = authenticate()
-getUserCreds()
+authenticate()
